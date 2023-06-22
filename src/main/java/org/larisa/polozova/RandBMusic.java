fd3f7c06cbe717db10c6a7dbb9e8ad683aeb28c4
Лариса <larolimp@gmail.com>
2023-06-22 15:08:13 +0300

    @Scope("prototype")

--- src/main/java/org/larisa/polozova/RandBMusic.java
+++ src/main/java/org/larisa/polozova/RandBMusic.java
@@ -1,7 +1,5 @@
 package org.larisa.polozova;
 
-import org.springframework.stereotype.Component;
-
 // id компонента бина
 public class RandBMusic implements Music {
     @Override
--- src/main/java/org/larisa/polozova/RockMusic.java
+++ src/main/java/org/larisa/polozova/RockMusic.java
@@ -2,10 +2,15 @@ package org.larisa.polozova;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class RockMusic implements Music{
     @Override
     public String getSong() {
-        return "Wind cries Mary";
+
+
+        return "Mary";
     }
 }
