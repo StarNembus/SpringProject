45d49d19df8b3b44ce61b3102e17f14ae01c3c20
Лариса <larolimp@gmail.com>
2023-06-22 16:14:05 +0300

    Java код

--- src/main/java/org/larisa/polozova/ClassicalMusic.java
+++ src/main/java/org/larisa/polozova/ClassicalMusic.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Scope("prototype")
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
--- src/main/java/org/larisa/polozova/TestSpring.java
+++ src/main/java/org/larisa/polozova/TestSpring.java
@@ -1,25 +1,14 @@
 package org.larisa.polozova;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        //обращается к файлу applicationContext.xml и считывает все бины
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-//        Music music  = context.getBean("rockMusic", Music.class);
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-//        musicPlayer.playMusic();
-//
-//        Music music1 = context.getBean("classicalMusic", Music.class);
-//        MusicPlayer classicalMusicPlayer = new MusicPlayer(music1);
-//        classicalMusicPlayer.playMusic();
 
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
-//        Computer computer = context.getBean("computer", Computer.class);
-//        System.out.println(computer);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
